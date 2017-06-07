6c63e0ff975503865bbc03d4ad176490e0e47a66
shermann <shermann@pc-dg-039-09>
2017-06-07 09:04:02 +0200

    Merge origin/master
    Conflicts:
    	src/Controleur/Controleur.java

--- src/Vue/VuePlateau.java
+++ src/Vue/VuePlateau.java
@@ -10,6 +10,7 @@ import Modèle.*;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.ArrayList;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,7 +27,7 @@ public class VuePlateau {
     public VuePlateau() {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        window.setSize(800, 600);
+        window.setSize(1024, 768);
         window.setBackground(Color.BLUE);
         
         ArrayList<JPanel> labelTuiles = new ArrayList<>();
@@ -34,26 +35,39 @@ public class VuePlateau {
         JPanel mapPanel = new JPanel(new GridLayout(6,6));
         window.add(mapPanel);
         Color colorBack;
-        for (int x=0; x<5; x++) {
-            for (int y=0; y<5; y++) {
+        for (int x=0; x<=5; x++) {
+            for (int y=0; y<=5; y++) {
                 //Tuile t = controleur.getGrille().getTuile(x, y);
                 // Test
                 Grille g = new Grille();
                 Tuile t = g.getTuile(x,y);
                 //fin du test
-                if (t==null || t.getEtat()==Etat.Sombré) {
+                if (t==null) {
                     labelTuiles.add(new JPanel());
-                    labelTuiles.get(labelTuiles.size()-1).setBackground(Color.blue);
+                    labelTuiles.get(labelTuiles.size()-1).setBackground(new Color(0,191,255)); //deepsky blue
+                } else if (t.getEtat()==Etat.Sombré) {
+                    labelTuiles.add(new JPanel());
+                    labelTuiles.get(labelTuiles.size()-1).setBackground(Color.LIGHT_GRAY);
+                    labelTuiles.get(labelTuiles.size()-1).setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                    labelTuiles.get(labelTuiles.size()-1).add(new JLabel(t.getIntitule().toString()));
                 } else {
                     if (t.getEtat()==Etat.Inondé) {
-                        colorBack = Color.BLUE;
+                        colorBack = new Color(10,110,230); //bleu clair
                     } else {
-                        colorBack = Color.YELLOW;
+                        colorBack = new Color(240,230,140); //jaune sable
                     }
                     labelTuiles.add(new JPanel());
                     labelTuiles.get(labelTuiles.size()-1).setBackground(colorBack);
                     labelTuiles.get(labelTuiles.size()-1).add(new JLabel(t.getIntitule().toString()));
+                    labelTuiles.get(labelTuiles.size()-1).setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                    if (t.getIntitule() == Zone.Heliport) {
+                       labelTuiles.get(labelTuiles.size()-1).setBorder(BorderFactory.createLineBorder(Color.RED));
+                    }
                 }
+                if (t.getReliqueDispo() != null) {
+                    
+                }
+                
             }
         }
         for (JPanel jpp: labelTuiles) {
