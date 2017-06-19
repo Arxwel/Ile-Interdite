756e7bf4ea945eeebd0cea2a6709025e2cfe0cd1
pasdelor <pasdelor@pc-dg-035-07>
2017-06-19 08:24:34 +0200

    Merge origin/master
    Conflicts:
    	src/Controleur/Controleur.java

--- src/Vue/VuePlateau.java
+++ src/Vue/VuePlateau.java
@@ -80,7 +80,7 @@ public class VuePlateau {
                     ImageIcon icon = new ImageIcon(t.getImage().getImage().getScaledInstance(170,170, Image.SCALE_DEFAULT));
                     caseTuiles.get(caseTuiles.size()-1).add(new JLabel(icon),BorderLayout.CENTER);
                     caseTuiles.get(caseTuiles.size()-1).add(new JLabel(t.getIntitule().nomEspace()),BorderLayout.SOUTH);
-                    caseTuiles.get(caseTuiles.size()-1).add(new JLabel("          "),BorderLayout.NORTH);
+                    //caseTuiles.get(caseTuiles.size()-1).add(new JLabel("          "),BorderLayout.NORTH);
                     //caseTuiles.get(caseTuiles.size()-1).add(upGridPanels.get(caseTuiles.size()-1),BorderLayout.NORTH);
                     //caseTuiles.get(caseTuiles.size()-1).add(downGridPanels.get(caseTuiles.size()-1),BorderLayout.SOUTH);
                     
