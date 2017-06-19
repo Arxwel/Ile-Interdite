63a650baad7e337dd1f103f300ed023f20d791df
shermann <shermann@pc-dg-039-09>
2017-06-19 10:06:18 +0200

    Merge origin/master
    Conflicts:
    	src/Vue/VueAventurier.java

--- src/Modèle/Grille.java
+++ src/Modèle/Grille.java
@@ -1,5 +1,6 @@
 package Modèle;
 
+import java.awt.Color;
 import java.util.*;
 import java.util.Arrays;
 
@@ -84,28 +85,28 @@ public class Grille {
         //Tuiles existantes
         tuiles[0][2] = new Tuile(Zone.LePontDesAbimes, Etat.Sec, this, new Coordonnees(0,2));
         tuiles[0][3] = new Tuile(Zone.LaPorteDeBronze, Etat.Inondé, this, new Coordonnees(0,3));
-        tuiles[1][1] = new Tuile(Zone.LaCaverneDesOmbres, Etat.Sec, this, new Coordonnees(1,1));
+        tuiles[1][1] = new Tuile(Zone.LaCaverneDesOmbres, Etat.Sec, this, new Coordonnees(1,1), Color.MAGENTA);
         tuiles[1][2] = new Tuile(Zone.LaPorteDeFer, Etat.Sec, this, new Coordonnees(1,2));
         tuiles[1][3] = new Tuile(Zone.LaPorteDOr, Etat.Sec, this, new Coordonnees(1,3));
         tuiles[1][4] = new Tuile(Zone.LesFalaisesDeLOubli, Etat.Sec, this, new Coordonnees(1,4));
-        tuiles[2][0] = new Tuile(Zone.LePalaisDeCorail, Etat.Sec, this, new Coordonnees(2,0));
+        tuiles[2][0] = new Tuile(Zone.LePalaisDeCorail, Etat.Sec, this, new Coordonnees(2,0), Color.CYAN);
         tuiles[2][1] = new Tuile(Zone.LaPorteDArgent, Etat.Sec, this, new Coordonnees(2,1));
         tuiles[2][2] = new Tuile(Zone.LesDunesDeLIllusion, Etat.Sombré, this, new Coordonnees(2,2));
         tuiles[2][3] = new Tuile(Zone.Heliport, Etat.Sec, this, new Coordonnees(2,3));
         tuiles[2][4] = new Tuile(Zone.LaPorteDeCuivre, Etat.Sec, this, new Coordonnees(2,4));
-        tuiles[2][5] = new Tuile(Zone.LeJardinDesHurlements, Etat.Sec, this, new Coordonnees(2,5));
+        tuiles[2][5] = new Tuile(Zone.LeJardinDesHurlements, Etat.Sec, this, new Coordonnees(2,5), Color.ORANGE);
         tuiles[3][0] = new Tuile(Zone.LaForetPourpre, Etat.Sec, this, new Coordonnees(3,0));
         tuiles[3][1] = new Tuile(Zone.LeLagonPerdu, Etat.Inondé, this, new Coordonnees(3,1));
         tuiles[3][2] = new Tuile(Zone.LeMaraisBrumeux, Etat.Sombré, this, new Coordonnees(3,2));
         tuiles[3][3] = new Tuile(Zone.Observatoire, Etat.Inondé, this, new Coordonnees(3,3));
         tuiles[3][4] = new Tuile(Zone.LeRocherFantome, Etat.Sombré, this, new Coordonnees(3,4));
-        tuiles[3][5] = new Tuile(Zone.LaCaverneDuBrasier, Etat.Inondé, this, new Coordonnees(3,5));
-        tuiles[4][1] = new Tuile(Zone.LeTempleDuSoleil, Etat.Sec, this, new Coordonnees(4,1));
-        tuiles[4][2] = new Tuile(Zone.LeTempleDeLaLune, Etat.Sombré, this, new Coordonnees(4,2));
-        tuiles[4][3] = new Tuile(Zone.LePalaisDesMarees, Etat.Sec, this, new Coordonnees(4,3));
+        tuiles[3][5] = new Tuile(Zone.LaCaverneDuBrasier, Etat.Inondé, this, new Coordonnees(3,5), Color.MAGENTA);
+        tuiles[4][1] = new Tuile(Zone.LeTempleDuSoleil, Etat.Sec, this, new Coordonnees(4,1), Color.GRAY);
+        tuiles[4][2] = new Tuile(Zone.LeTempleDeLaLune, Etat.Sombré, this, new Coordonnees(4,2), Color.GRAY);
+        tuiles[4][3] = new Tuile(Zone.LePalaisDesMarees, Etat.Sec, this, new Coordonnees(4,3), Color.CYAN);
         tuiles[4][4] = new Tuile(Zone.LeValDuCrepuscule, Etat.Sec, this, new Coordonnees(4,4));
         tuiles[5][2] = new Tuile(Zone.LaTourDuGuet, Etat.Sec, this, new Coordonnees(5,2));
-        tuiles[5][3] = new Tuile(Zone.LeJardinDesMurmures, Etat.Inondé, this, new Coordonnees(5,3));
+        tuiles[5][3] = new Tuile(Zone.LeJardinDesMurmures, Etat.Inondé, this, new Coordonnees(5,3), Color.ORANGE);
         
     }
 
